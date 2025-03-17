c16233ef57c1660278d1ad4ab1b9ef97f8ed3ae5
Jane Zdravevski <jane@zdravevski.pro>
2025-03-17 15:24:59 +0100

    added basic router structure, created all pages I'll need, created Login/Register page, added custom class for aligning the page content with tailwind, etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,72 @@
+import {
+    HomeLayout,
+    Landing,
+    Error,
+    Products,
+    SingleProduct,
+    Cart,
+    About,
+    Register,
+    Login,
+    Checkout,
+    Orders,
+} from "./pages";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomeLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Landing />,
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+            {
+                path: "products",
+                element: <Products />,
+            },
+            {
+                path: "product/:id",
+                element: <SingleProduct />,
+            },
+            {
+                path: "cart",
+                element: <Cart />,
+            },
+            {
+                path: "checkout",
+                element: <Checkout />,
+            },
+            {
+                path: "Orders",
+                element: <Orders />,
+            },
+        ],
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+]);
+
 const App = () => {
     return (
-        <div>
-            <h1 className="prose-2xl">Hello World!</h1>
-            <button className="btn btn-soft btn-secondary">Click Me</button>
-        </div>
+        <RouterProvider router={router}>
+            <div>
+                <h1 className="prose-2xl">Hello World!</h1>
+                <button className="btn btn-soft btn-secondary">Click Me</button>
+            </div>
+        </RouterProvider>
     );
 };
 export default App;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet, Link } from "react-router-dom";
+
+const HomeLayout = () => {
+    return (
+        <>
+            <nav>
+                <h1 className="text-4xl text-primary">Comfy</h1>
+            </nav>
+            <Outlet />
+        </>
+    );
+};
+
+export default HomeLayout;
