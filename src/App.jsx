5ee854f0909421c41aed2f16a616ca0369b1d06d
Jane Zdravevski <jane@zdravevski.pro>
2025-03-17 13:28:38 +0100

    downgraded tailwind to v3 (coz plugins like typography is not working in v4), and added daisyui in the config as plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 const App = () => {
-    return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+    return (
+        <div>
+            <h1 className="prose-2xl">Hello World!</h1>
+            <button className="btn btn-secondary">Click Me</button>
+        </div>
+    );
 };
 export default App;
