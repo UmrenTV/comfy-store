9fbf3183b8d2438b16989a50b1624acef420ef48
Jane Zdravevski <jane@zdravevski.pro>
2025-03-17 16:30:01 +0100

    added header, navbar, theme switcher, downgraded daisyui (themes were not working), etc

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
+import { Header, Navbar } from "../components";
 
 const HomeLayout = () => {
     return (
         <>
-            <nav>
-                <h1 className="text-4xl text-primary">Comfy</h1>
-            </nav>
-            <Outlet />
+            <Header />
+            <Navbar />
+            <section className="align-element py-20">
+                <Outlet />
+            </section>
         </>
     );
 };
