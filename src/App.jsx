5495f09068f2c6fc4b2659ac8f31189a8fc8ef44
Jane Zdravevski <jane@zdravevski.pro>
2025-03-17 13:55:34 +0100

    added bunch of dependancies like router, react-query, redux toolkit, toastify, dayjs, react-icons, etc

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
     return (
         <div>
             <h1 className="prose-2xl">Hello World!</h1>
-            <button className="btn btn-secondary">Click Me</button>
+            <button className="btn btn-soft btn-secondary">Click Me</button>
         </div>
     );
 };
