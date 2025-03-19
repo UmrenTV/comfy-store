bafe72af86299deecf2f9ccdf930311fa4b5836c
Jane Zdravevski <jane@zdravevski.pro>
2025-03-19 18:20:12 +0100

    filtering is done, added Loading spinner as well for navigation / search - NEXT: Pagination

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,10 +5,15 @@ import { useLoaderData } from "react-router-dom";
 const url = "products";
 
 export const loader = async ({ request }) => {
-    const response = await customFetch(url);
+    // const params = new URL(request.url).searchParams;
+    // const search = params.get("search"); // this is approach for one by one
+    const params = Object.fromEntries([
+        ...new URL(request.url).searchParams.entries(),
+    ]); // this is a one liner approach
+    const response = await customFetch(url, { params }); // then we can just pass them as object named params, coz axios expects params object anyways.
     const products = response.data.data;
     const meta = response.data.meta;
-    return { products, meta };
+    return { products, meta, params };
 };
 
 const Products = () => {
